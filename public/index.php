bc84fbb71f66e4b904276ec0e825ad265df6f98f
Oriol Chiva <adaw23ochiva@inslaferria.cat>
2025-10-21 10:37:16 +0200

    Actualizar index.php para probar BD y workflow completo

--- public/index.php
+++ public/index.php
@@ -11,6 +11,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("<h1>Connexió fallida: " . $conn->connect_error . "</h1>");
 }
+echo "<h1> Felicidades!</h1>";
 echo "<h1>Connexió exitosa a la Base de Dades!</h1>";
 
 $conn->close();
