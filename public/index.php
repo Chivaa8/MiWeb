93f207bfc121c0e70fe5252f834dc3ac1f2bdcd4
Oriol Chiva <adaw23ochiva@inslaferria.cat>
2025-10-21 10:29:48 +0200

    Actualizar index.php para probar BD y workflow completo

--- public/index.php
+++ public/index.php
@@ -1,18 +1,17 @@
 <?php
-$servername = "db";      // Nom del servei MySQL a docker-compose
-$username = "user";      
+$servername = "db";
+$username = "user";
 $password = "password";
 $dbname = "demo";
 
-// Crear connexió
+// Crear conexió
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Comprovar connexió
 if ($conn->connect_error) {
-    die("Connexió fallida: " . $conn->connect_error);
+    die("<h1>Connexió fallida: " . $conn->connect_error . "</h1>");
 }
 echo "<h1>Connexió exitosa a la Base de Dades!</h1>";
 
-// Tancar connexió
 $conn->close();
 ?>
