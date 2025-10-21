366d773ab8361783843da58ae0a2b1f228870f8e
Oriol Chiva <adaw23ochiva@inslaferria.cat>
2025-10-21 10:24:17 +0200

    conectandose a la BD

--- public/index.php
+++ public/index.php
@@ -1,5 +1,18 @@
-<?
-echo "<h1>Hola Mön</h1>";
-?>
+<?php
+$servername = "db";      // Nom del servei MySQL a docker-compose
+$username = "user";      
+$password = "password";
+$dbname = "demo";
 
-<? echo "A por otro push jeje" ?>
+// Crear connexió
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Comprovar connexió
+if ($conn->connect_error) {
+    die("Connexió fallida: " . $conn->connect_error);
+}
+echo "<h1>Connexió exitosa a la Base de Dades!</h1>";
+
+// Tancar connexió
+$conn->close();
+?>
